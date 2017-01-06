1a32ba9660d2fc5e49ca62983fc2b24b0d209820
GYCHEN <gy.chen@gms.nutc.edu.tw>
2017-01-06 09:45:55 +0000

    fix type and MULTIPLE_NUM

--- idvalidator.py
+++ idvalidator.py
@@ -7,7 +7,7 @@ class IdValidator:
     The second code must be 1 or 2.
     """
 
-    MULTIPLE_NUM = (1, 1, 1, 2, 0, 8, 6, 3, 1, 5, 8)
+    MULTIPLE_NUM = (1, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1)
 
     def validate(self, personal_id):
         if len(personal_id) != 10:
@@ -25,10 +25,10 @@ class IdValidator:
             multiple_codes.append(int(code))
         multiple_sum = 0
         for multiple_code, code in zip(multiple_codes, self.MULTIPLE_NUM):
-            multiple_sum += multiple_sum * code
+            multiple_sum += multiple_code * code
         return multiple_sum % 10 == 0
 
 if __name__ == '__main__':
-    test_id = 'B120863158'
+    test_id = 'B122933877'
     validator = IdValidator()
     print(validator.validate(test_id))
